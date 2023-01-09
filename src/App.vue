251f9aee6978a3e55079e0c3b34baf0f4e5f849e
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-01-09 15:37:03 +0100

    start the header

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import DCFooter from './components/DCFooter.vue';
+import DCHeader from './components/DCHeader.vue';
+import DCMain from './components/DCMain.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header>
+    <DCHeader />
+  </header>
+
+  <main>
+    <DCMain />
+  </main>
+  
+  <footer>
+    <DCFooter />
+  </footer>
+
 </template>
 
 <style lang="scss">
+@use './styles/general';
+@use './styles/partials/variable';
+@use './bootstrap/scss/bootstrap.scss'
 
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ src/components/DCHeader.vue
@@ -0,0 +1,68 @@
+<template>
+    <div id="footer-container" class="container-fluid">
+        <div class="logo">
+
+        </div>
+
+        <div class="navbar">
+            <ul>
+                <li>
+                    characters
+                </li>
+                <li>
+                    comics
+                </li>
+                <li>
+                    movies
+                </li>
+                <li>
+                    tv
+                </li>
+                <li>
+                    games
+                </li>
+                <li>
+                    collectibles
+                </li>
+                <li>
+                    videos
+                </li>
+                <li>
+                    fans
+                </li>
+                <li>
+                    news
+                </li>
+                <li>
+                    shop
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+    #footer-container{
+        height: 150px;
+        width: 100%;
+        background-color: black;
+
+        .logo{
+            width: 100px;
+            height: 100%;
+            background-color: green;
+        }
+
+    }
+
+
+
+
+
+</style>
+
+<script>
+export default {
+    
+}
+</script>

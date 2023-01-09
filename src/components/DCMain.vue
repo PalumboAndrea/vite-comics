8178419ff3198e00cf968c2eb7dd186334780ae3
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-01-09 15:45:11 +0100

    add main structure

--- src/components/DCMain.vue
+++ src/components/DCMain.vue
@@ -1,13 +1,32 @@
 <template>
-    <div>
+    <div id="content-container">
+        
+    </div>
+    <div id="banner">
         
     </div>
 </template>
+
+<style lang="scss">
+    #content-container{
+        background-color: rgb(28,28,28);
+        width: 100%;
+        height: 150px;
+    }
+
+    #banner{
+        height: 160px;
+        width: 100%;
+        background-color: rgb(1,130,249);
+    }
+
+
+
+</style>
+
 <script>
 export default {
     
 }
 </script>
-<style lang="">
-    
-</style>
+

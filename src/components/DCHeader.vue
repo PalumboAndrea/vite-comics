4c251c191450aa056942c5c7e0f51e7c40af9a2e
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-01-09 15:41:31 +0100

    add header structure

--- src/components/DCHeader.vue
+++ src/components/DCHeader.vue
@@ -43,14 +43,28 @@
 
 <style lang="scss">
     #footer-container{
+       
         height: 150px;
-        width: 100%;
-        background-color: black;
+        background-color: white;
+        display: flex;
+        justify-content: space-between;
 
         .logo{
             width: 100px;
             height: 100%;
             background-color: green;
+        }
+
+        ul{
+            list-style-type: none;
+            padding: 0;
+            display: flex;
+            flex-direction: row;
+            color: rgb(70,70,70);
+
+            li{
+                padding: 0 10px;
+            }
         }
 
     }
